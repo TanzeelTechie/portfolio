0cb0750f54f311443ec11062044c5b59a8a31b5b
TanzeelTechie <tanzeel31vlr@gmail.com>
2025-01-18 19:39:16 +0530

    main.js file added

---
+++ main.js
@@ -0,0 +1,80 @@
+const navLinks = document.getElementById("nav-links");
+const menuBtn = document.getElementById("menu-btn");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "top",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__image",{
+  ...scrollRevealOption,
+  delay:800,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// about container
+ScrollReveal().reveal(".about__image",{
+  ...scrollRevealOption,
+  delay:300,
+});
+
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .about__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// service container
+ScrollReveal().reveal(".service__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// skill container
+ScrollReveal().reveal(".skill__card",{
+  interval: 500,
+});
+
+//contact form
+ScrollReveal().reveal(".contact__input",{
+  duration: 1000,
+  interval: 500,
+});
